29a8ec4e8415ad9a6c1efc5b46154b0826f48b3e
misumi-pham-thanhlong-01 <thanhlong.e574.pham@misumi.co.jp>
2025-05-21 23:24:47 +0900

    [Common] Setup enviroment

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+};
